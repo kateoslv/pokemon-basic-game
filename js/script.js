424d2ef5d8d487999e5ca5f18d43ea79ebd288bb
kateoslv <kattyolv@gmail.com>
2023-10-10 23:51:41 +0100

    Ash character movement manipulation

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const body = document.querySelector("body");
+const ash = document.querySelector("#ash");
+
+function getRightPosition() {
+    return parseInt(ash.style.right.split("px")) || 2;
+}
+
+function getTopPosition() {
+    return parseInt(ash.style.top.split("px")) || 2;
+}
+
+body.addEventListener("keydown", (event) => {
+    event.stopPropagation();
+
+    switch (event.code) {
+        case 'ArrowLeft':
+            if (getRightPosition() < 770) {
+                ash.style.right = `${getRightPosition() + 8}px`;
+                ash.src = "../assets/left.png";
+            }
+            break;
+        case 'ArrowRight':
+            if (getRightPosition() > 2) {
+                ash.style.right = `${getRightPosition() - 8}px`;
+                ash.src = "../assets/right.png";
+            }
+            break;
+        case 'ArrowDown':
+            if (getTopPosition() < 625) {
+                ash.style.top = `${getTopPosition() + 8}px`;
+                ash.src = "../assets/front.png";
+            }
+            break;
+        case 'ArrowUp':
+            if (getTopPosition() > 2) {
+                ash.style.top = `${getTopPosition() - 8}px`;
+                ash.src = "../assets/back.png";
+            }
+            break;
+        default:
+            break;
+    }
+});
